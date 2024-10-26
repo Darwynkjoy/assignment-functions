726fd2e9f5a29292a894d57aa9197416cd7d7908
darwynkjoy <darwynkjoy@gmail.com>
2024-10-26 13:33:12 +0530

    daily push

---
+++ createuser.dart
@@ -0,0 +1,8 @@
+void createUser(String name,int age,{String isActive="active"}){
+    print("name:$name");
+    print("age:$age");
+    print("status:$isActive");
+    }
+void main(){
+    createUser("john",25);
+    }
---
+++ rectangle.dart
@@ -0,0 +1,7 @@
+int calculateArea({int length=1,int breadth=1})=>
+    length*breadth;
+
+void main(){
+    int area=calculateArea();
+    print("area of the reactangle l=1,b=1 =$area");
+    }
